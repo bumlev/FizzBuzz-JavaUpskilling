7de8c68fe8c7db68bafeabd2b97963e6693a9f6e
bumlev <levy.ubumwe@amalitech.com>
2024-02-08 20:40:33 +0200

    multiple numbers by 3 and 5

--- src/Execution.java
+++ src/Execution.java
@@ -1,2 +1,9 @@
 public class Execution {
+
+    public static void main(String[] args) {
+
+        MultipleNumber multipleNumber = new MultipleNumber();
+        multipleNumber.inputtingNumbers();
+        multipleNumber.displayFizzOrBuzzState();
+    }
 }
